6d075dcecd24447a42158dcc89e495fa41eb5bee
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-06-26 22:38:38 -0500

    Action Result Callbacks Added a way for the Action Callback abstractions to return a result during the call.

---
+++ EventHorizon.Blazor.Interop.Sample/Pages/Testing/InteropTesting/Model/TestClass.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EventHorizon.Blazor.Interop.Sample.Pages.Testing.InteropTesting.Model
+{
+    [JsonConverter(typeof(CachedEntityConverter<TestClass>))]
+    public class TestClass : CachedEntity
+    {
+        public bool Found
+        {
+            get
+            {
+                return EventHorizonBlazorInterop.Get<bool>(
+                    this.___guid,
+                    "found"
+                );
+            }
+            set
+            {
+                EventHorizonBlazorInterop.Set(
+                    this.___guid,
+                    "found",
+                    value
+                );
+            }
+        }
+
+        public string Arg1
+        {
+            get
+            {
+                return EventHorizonBlazorInterop.Get<string>(
+                    this.___guid,
+                    "arg1"
+                );
+            }
+            set
+            {
+                EventHorizonBlazorInterop.Set(
+                    this.___guid,
+                    "arg1",
+                    value
+                );
+            }
+        }
+
+        public TestClass()
+        {
+            var cachedEntity = EventHorizonBlazorInterop.New(
+                new string[] { "Vector3" },
+                1,
+                2,
+                3
+            );
+            ___guid = cachedEntity.___guid;
+        }
+    }
+}
---
+++ EventHorizon.Blazor.Interop/ResultCallbacks/ActionResultCallback.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.JSInterop;
+using System;
+
+namespace EventHorizon.Blazor.Interop.ResultCallbacks
+{
+    /// <summary>
+    /// A platform provided abstraction to help with creation of a action that will trigger when called from the Client side.
+    /// Includes Result when Called
+    /// </summary>
+    public class ActionResultCallback<TResult>
+    {
+        /// <summary>
+        /// This is a type that gets passed to the Client side to help with the client side marshal of arguments.
+        /// </summary>
+        public string ___type => "action_result_callback";
+        /// <summary>
+        /// The .NET representation that the Client will call back to on Action trigger.
+        /// </summary>
+        public DotNetObjectReference<ActionResultCallback<TResult>> invokableReference { get; }
+        /// <summary>
+        /// The method on the <see cref="invokableReference" /> that will be called when the Action is triggered.
+        /// </summary>
+        public string method => "HandleCallback";
+
+        private Func<TResult> _callback;
+
+        /// <summary>
+        /// Create a new Action callback representation that will be triggered when the Client calls the method.
+        /// </summary>
+        /// <param name="callback">The custom action that should be triggered.</param>
+        public ActionResultCallback(
+            Func<TResult> callback
+        )
+        {
+            _callback = callback;
+            invokableReference = DotNetObjectReference.Create(
+                this
+            );
+        }
+
+        /// <summary>
+        /// The public method that will be called by the Client when an Action should be triggered.
+        /// </summary>
+        /// <returns>Make this method async for usage with Client side.</returns>
+        [JSInvokable]
+        public TResult HandleCallback()
+        {
+            return _callback();
+        }
+    }
+}
