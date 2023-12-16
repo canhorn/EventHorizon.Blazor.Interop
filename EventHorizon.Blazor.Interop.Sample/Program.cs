e635c661c8f8c2674d0978b3410a1b03b67c5772
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2023-12-16 16:48:10 -0600

    feat(upgrade): Upgraded to .NET8 Migrated to new Interop patterns. Expanded on Performance Validations.

--- EventHorizon.Blazor.Interop.Sample/Program.cs
+++ EventHorizon.Blazor.Interop.Sample/Program.cs
@@ -1,39 +1,29 @@
+namespace EventHorizon.Blazor.Interop.Sample;
+
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using MediatR;
 using EventHorizon.Blazor.Interop.Sample.Pages.DITesting.Model;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace EventHorizon.Blazor.Interop.Sample
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("app");
 
-            builder.Services
-                .AddTransient(
-                    sp => new HttpClient
-                    {
-                        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-                    }
-                );
-            builder.Services
-                .AddSingleton<IDIRunHandler, DIRunHandlerImplementation>();
-            builder.Services
-                .AddMediatR(
-                    typeof(Program).Assembly
-                );
+        builder
+            .Services
+            .AddTransient(
+                sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
+            );
+        builder.Services.AddSingleton<IDIRunHandler, DIRunHandlerImplementation>();
+        builder
+            .Services
+            .AddMediatR(opts => opts.RegisterServicesFromAssemblies(typeof(Program).Assembly));
 
-            await builder.Build().RunAsync();
-        }
+        await builder.Build().RunAsync();
     }
 }
